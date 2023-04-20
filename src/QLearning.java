6c9635b359aaf5cbf5236076a2bafa08e17b7bd7
lillyphan <lphan24@kentdenver.org>
2023-04-20 10:14:12 -0600

    QLearning intialization and declarations

--- src/QLearning.java
+++ src/QLearning.java
@@ -1,2 +1,13 @@
+import java.util.*;
+
 public class QLearning {
+    public static void main(String[] args) {
+        Maze m;
+        double[][] R;
+        double[][] Q;
+
+        m = new Maze(15, 15, new int[]{0, 0}, new int[]{14, 14});
+        R = new double[15][15];
+        Q = new double[15][15];
+    }
 }
